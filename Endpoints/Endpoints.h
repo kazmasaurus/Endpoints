70f99bdb0929ab65db00a386c4dd2173b64808fa
Zak Remer <zak.remer@gmail.com>
2016-08-15 15:29:39 -0700

    Initial Commit.

---
+++ Endpoints/Endpoints.h
@@ -0,0 +1,19 @@
+//
+//  Endpoints.h
+//  Endpoints
+//
+//  Created by Zak Remer on 8/10/16.
+//  Copyright © 2016 Opal. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Endpoints.
+FOUNDATION_EXPORT double EndpointsVersionNumber;
+
+//! Project version string for Endpoints.
+FOUNDATION_EXPORT const unsigned char EndpointsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Endpoints/PublicHeader.h>
+
+
