e4059f946d7d11dee29ab96ed3d247c218364e36
Zak Remer <zak.remer@gmail.com>
2016-08-19 10:45:36 -0700

    Exploding some definitions.

--- Endpoints/Models.swift
+++ Endpoints/Models.swift
@@ -13,34 +13,50 @@ import Foundation
 
 /// The head matter for a given object.
 /// Probably just the id, but a place to add stuff like `created_at`
-struct Head { var id: String }
+public struct Head {
+    public var id: String
+}
 
 // MARK: Relationships
 
 /// A relationship pointer.
-struct Pointer { var id: String; var type: String }
+public struct Pointer {
+    public var id: String
+    public var type: String
+}
+
+extension Pointer: Equatable {
+    public static func == (lhs: Pointer, rhs: Pointer) -> Bool {
+        return lhs.id == rhs.id && lhs.type == rhs.type
+    }
+}
 
 /// A relationship object.
 /// Contains the information about the relationship,
 /// resource locations for the related resource,
 /// and the related object itself if it's been fetched.
-struct To<Relationship: RelationshipType> {
+public struct To<Relationship: RelationshipType> {
     /// Links would go here.
-    var friend: Related<Relationship>
-}
+    public var data: Related<Relationship>
 
-/// Abstract ways to have relationships.
-/// These are generally uninitable, and simply exist to define the types that could be found in the relationship value.
-protocol RelationshipType { associatedtype PointerType; associatedtype ElementType }
-enum One<Element>: RelationshipType { typealias PointerType = Pointer; typealias ElementType = Element }
-enum Many<Element>: RelationshipType { typealias PointerType = Array<Pointer>; typealias ElementType = Array<Element> }
-enum Maybe<Element>: RelationshipType { typealias PointerType = Optional<Pointer>; typealias ElementType = Optional<Pointer> }
+    public init(_ pointer: Relationship.PointerType) {
+        self.data = .unfetched(pointer)
+    }
+
+    public init(_ model: Relationship.ElementType) {
+        self.data = .fetched(model)
+    }
+
+    public init(data: Related<Relationship>) {
+        self.data = data
+    }
+}
 
 /// Describes the possible states of a relationship:
 /// - unknown: There's not yet any information available about the relationship
 /// - unfetched: We know the id(s) of any objects included in the relationship
 /// - fetched: We know the actual data for the objects in the relationship
-enum Related<Relationship: RelationshipType> {
+public enum Related<Relationship: RelationshipType> {
     // When the relationship object has no data.
     // As much as I would like to avoid it, it's technically possible in the spec,
     // And I have no reason to believe that we won't end up with it in the API.
@@ -55,6 +71,44 @@ enum Related<Relationship: RelationshipType> {
 
     // Do we want to spend any effort thinking about if the relationship is paged?
     // (Probably not, but presumably you could double check with someone.)
+}
+
+typealias ToOne<Element> = To<One<Element>>
+typealias ToMany<Element> = To<Many<Element>>
+typealias ToMaybe<Element> = To<Maybe<Element>>
+
+public extension Related {
+    public var asUnfetched: Relationship.PointerType? {
+        guard case let .unfetched(pointer) = self else { return nil }
+        return pointer
+    }
+
+    public var asFetched: Relationship.ElementType? {
+        guard case let .fetched(element) = self else { return nil }
+        return element
+    }
+}
+
+/// Abstract ways to have relationships.
+/// These are generally uninitable, and simply exist to define the types that could be found in the relationship value.
+public protocol RelationshipType {
+    associatedtype PointerType
+    associatedtype ElementType
+}
+
+public enum One<Element>: RelationshipType {
+    public typealias PointerType = Pointer
+    public typealias ElementType = Element
+}
+
+public enum Many<Element>: RelationshipType {
+    public typealias PointerType = Array<Pointer>
+    public typealias ElementType = Array<Element>
+}
+
+public enum Maybe<Element>: RelationshipType {
+    public typealias PointerType = Optional<Pointer>
+    public typealias ElementType = Optional<Element>
 }
 
 // MARK: - FromJSONification
@@ -76,8 +130,8 @@ struct Author {
     var birth: Date
     var death: Date?
 
-    var books: To<Many<Book>>
-    var photos: To<Many<Photo>>
+    var bookRelationships: ToMany<Book>
+    var photoRelationshipss: ToMany<Photo>
 }
 
 struct Book {
@@ -87,12 +141,12 @@ struct Book {
     var title: String
     var published: Date
 
-    var author: To<One<Author>>
-    var series: To<Maybe<Series>>
+    var authorRelationship: ToOne<Author>
+    var seriesRelationship: ToMaybe<Series>
 
-    var chapters: To<Many<Chapter>>
-    var photos: To<Many<Photo>>
-    var stores: To<Many<Store>>
+    var chapterRelationships: ToMany<Chapter>
+    var photoRelationships: ToMany<Photo>
+    var storeRelationships: ToMany<Store>
 }
 
 struct Photo {
@@ -102,7 +156,7 @@ struct Photo {
     var title: String
     var uri: URL
 
-    var imageable: To<One<Imageable>>
+    var imageableRelationship: To<One<Imageable>>
 }
 
 enum Imageable {
@@ -117,7 +171,7 @@ struct Chapter {
     var title: String
     var ordering: Int
 
-    var book: To<One<Book>>
+    var bookRelationship: To<One<Book>>
 }
 
 struct Series {
@@ -125,7 +179,8 @@ struct Series {
     var head: Head
 
     var title: String
-    var books: To<Many<Book>>
+
+    var bookRelationships: To<Many<Book>>
 }
 
 struct Store {
@@ -133,7 +188,9 @@ struct Store {
 
     var name: String
 
-    var books: To<Many<Book>>
+    var books: [Book] { return bookRelationships.data.asFetched ?? [] }
+    // or var books: [Book]? ?
+    var bookRelationships: To<Many<Book>>
 }
 
 extension Store: FromJSONable {
@@ -145,7 +202,7 @@ extension Store: FromJSONable {
 
         self.head = Head(id: id)
         self.name = name
-        self.books = To(friend: .unknown)
+        self.bookRelationships = To(data: .unknown)
     }
 }
 
--- EndpointsTests/EndpointsTests.swift
+++ EndpointsTests/EndpointsTests.swift
@@ -18,6 +18,18 @@ class EndpointsTests: XCTestCase {
         XCTAssertEqual(store?.head.id, "1")
         XCTAssertEqual(store?.name, "empty store")
     }
+
+    func testFullStore() {
+        let data = datum(from: "FullStore".jsonFixture)!
+        let store = Store(json: data)
+        XCTAssertNotNil(store)
+        XCTAssertEqual(store?.head.id, "2")
+        XCTAssertEqual(store?.name, "full store")
+
+        let expectedPointers = Array<Int>(1...11).map { Pointer(id: String($0), type: "books") }
+        guard let pointers = store?.bookRelationships.data.asUnfetched else { XCTFail(); return }
+        XCTAssertEqual(pointers, expectedPointers)
+    }
 }
 
 func datum(from: JSON) -> JSON! { return from["data"] as? JSON }
